7aff6e10fbc11f08ca75c4eba6a3ae576b2f735b
Agustin Armendariz <aarmendariz@cironline.org>
2012-12-30 12:33:28 -0800

    couple easy scripts to show what's possible

---
+++ db_audit_trail.py
@@ -0,0 +1,175 @@
+import urllib
+import sqlite3
+import os
+import csv
+from datetime import datetime
+from nameparser import HumanName # nameparser module available here -- http://code.google.com/p/python-nameparser/
+
+
+## connect to an SQLite3 database
+conn = sqlite3.connect('contribs.sqlite', isolation_level=None) # creates the db file if it doesn't already exist. we set isolation_level to None so that we are in autocommit mode. that way the UPDATES are applied without further ado
+c = conn.cursor() # open the lines of communication with the database
+
+date_stamp = datetime.strftime(datetime.now(), '%Y%m%d')
+
+def fetch_data():
+    # set-up a date stamped folder to hold the data in
+    if os.path.isdir(date_stamp): # test to see if the folder already exists
+        try:
+            cmd = 'rm -rf %s' % date_stamp # try deleteing the folder
+            os.system(cmd)
+        except:
+            cmd = 'rmdir /S %s' % date_stamp # if the above fails try the Windows command line way. If both fail, the script fails
+            os.system(cmd)
+    
+    os.mkdir(date_stamp)
+    file_path = os.path.join(date_stamp, 'contribs.csv')
+    
+    ## fetch the data
+    url = 'http://cal-access.sos.ca.gov/Campaign/Committees/DetailContributionsReceivedExcel.aspx?id=1340976&session=2011'
+    try:
+        urllib.urlretrieve(url, file_path)
+        print 'fetched data, see %s' % file_path
+    except:
+        print 'failed to fetch data'
+
+def create_table():
+    file_path = os.path.join(date_stamp, 'contribs.csv')
+    infile = open(file_path) # open the csv file
+    handle = csv.reader(infile, delimiter='\t') # use the csv module to parse the csv
+    headers = handle.next() # grab the header row
+    infile.close() # close the csv file now that we're done
+    
+    ## test to see if the table already exists
+    sql_statement = '''
+        SELECT name FROM sqlite_master WHERE type='table' AND name="%s_raw_contribs"
+    ''' % date_stamp
+    cmd = c.execute(sql_statement)
+    results = cmd.fetchall()
+    if len(results) == 1:
+        print 'table already exists, nuking it'
+        sql_statement = "DROP TABLE `%s_raw_contribs`" % date_stamp
+        cmd = c.execute(sql_statement)
+    
+    ## create a table using the headers of the csv file
+    sql_statement = "CREATE TABLE `%s_raw_contribs` (" % date_stamp
+    
+    ## loop through the headers and use them to craft the rest of the sql statement
+    for i in range(len(headers)):
+        sql_friendly_header = headers[i].lower().replace(' ', '_') # index into the headers list, get rid of the spaces and change it from upper to lower case
+        if i == len(headers)-1:
+            creation_statement_piece = '%s TEXT' % sql_friendly_header # test to see if you are at the last element in the list. if you are, then you need to leave the comma out
+        else:
+            creation_statement_piece = '%s TEXT,' % sql_friendly_header
+        sql_statement += creation_statement_piece
+    sql_statement += ');' # put the finishing touch on the sql statement
+    c.execute(sql_statement) # use the sql statement we just wrote to create the table
+
+def load_data():
+    file_path = os.path.join(date_stamp, 'contribs.csv')
+    infile = open(file_path) # open the csv file
+    handle = csv.reader(infile, delimiter='\t') # use the csv module to parse the csv
+    headers = handle.next() # grab the header row
+    for h in handle: # loop through the data and insert it into the table
+        if len(h) == 12:
+            sql_statement = '''
+                INSERT INTO `%s_raw_contribs` VALUES( "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s" );
+            ''' % (date_stamp, h[0], h[1], h[2], h[3], h[4], h[5], h[6], h[7], h[8], h[9], h[10], h[11])
+            c.execute(sql_statement)
+        else:
+            print 'skipped row with %s columns' % len(h)
+    infile.close() # close the csv file now that we're done
+    print 'loaded up %s rows' % handle.line_num
+
+def clean_up_amount():
+    sql_query = 'ALTER TABLE `%s_raw_contribs` ADD COLUMN fltAmount DECIMAL(16,2)' % date_stamp # create a new column to house the numeric data. right now it saved as a string value
+    c.execute(sql_query)
+    sql_query = 'SELECT rowid, amount, fltAmount from `%s_raw_contribs`' % date_stamp # grab the rows, 
+    execute_query = c.execute(sql_query)
+    rows = execute_query.fetchall()
+    for row in rows:
+        new_amount = row[1].replace('$', '').replace(',', '')
+        #print '%s\t%s\t%s' % (row[0], row[1], new_amount) # i printed out the amounts while i was writing the script. no need for it now
+        sql_query = 'UPDATE `%s_raw_contribs` SET fltAmount = %s WHERE rowid= %s' % (date_stamp, new_amount, row[0])
+        c.execute(sql_query)
+
+def parse_names():
+    try:
+        new_field_list = ['title', 'fname', 'middle', 'lname', 'suffix', ]
+        for field in new_field_list:
+            sql_query = ''' ALTER TABLE `%s_raw_contribs` ADD %s TEXT  ''' % (date_stamp, field)
+            c.execute(sql_query)
+    except:
+        print 'fields already exist'
+    sql_select_command = ''' SELECT rowid, name_of_contributor FROM `%s_raw_contribs` WHERE employer <> '' AND occupation <> '' ''' % date_stamp # every sqlite table automatically has a unique row id you can use
+    execute_query = c.execute(sql_select_command)
+    rows = execute_query.fetchall()
+    for row in rows:
+        row_id = row[0]
+        name = row[1] # python lists start counting at 0 and the full name of the contributor is the second column in the sql select statement
+        parsed_name = HumanName(name)
+        sql_command = '''
+            UPDATE `%s_raw_contribs` SET title = '%s', fname = '%s', middle = '%s', lname = '%s', suffix = '%s' WHERE rowid = %s
+        ''' % (
+               date_stamp, 
+               parsed_name.title,
+               parsed_name.first,
+               parsed_name.middle,
+               parsed_name.last,
+               parsed_name.suffix,
+               row_id,
+               )
+        c.execute(sql_command)
+
+def look_at_those_purdy_names():
+    sql_command = "SELECT rowid, name_of_contributor, title, fname, middle, lname, suffix FROM `%s_raw_contribs`;" % (date_stamp)
+    execute_query = c.execute(sql_command)
+    row_list = execute_query.fetchall()
+    for row in row_list:
+        print '%s\t%s\t%s\t%s\t%s\t%s' % (row[0], row[1], row[2], row[3], row[4], row[5])
+
+def interesting_sum():
+    sql_command = '''
+        SELECT SUM(amount) FROM `%s_raw_contribs`
+    ''' % date_stamp
+    execute_query = c.execute(sql_command)
+    row_list = execute_query.fetchall()
+
+def return_all_rows():
+    sql_command = '''
+        SELECT * FROM `%s_raw_contribs`
+    ''' % date_stamp
+    execute_query = c.execute(sql_command)
+    row_list = execute_query.fetchall()
+    return row_list
+
+def do_it_do_it():
+    try:
+        fetch_data()
+        print 'fetched data'
+    except:
+        print 'failed to fetch data'
+    try:
+        create_table()
+        print 'created table'
+    except:
+        print 'failed to create table'
+    try:
+        load_data()
+        print 'loaded up the data'
+    except:
+        print 'failed to load the data'
+    try:
+        parse_names()
+        print 'parsed names'
+    except:
+        print 'failed to parse names'
+    try:
+        clean_up_amount()
+        print 'fixed the amounts'
+    except:
+        print 'failed to fix the amounts'
+
+
+if __name__ == '__main__':
+    do_it_do_it()
---
+++ grab_files_do_something.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+import requests # great library for doing stuff on the web
+import os, fnmatch
+import re
+
+def all_files(root, patterns='*', single_level=False, yield_folders=False):
+    # Taken from the O'Reilly Python Cookbook
+    # Expand patterns form semicolon-separated string to list
+    # example usage: thefiles = list(all_files('/tmp', '*.py;*.htm;*.html'))
+    patterns = patterns.split(';')
+    for path, subdirs, files in os.walk(root):
+        if yield_folders:
+            files.extend(subdirs)
+        files.sort()
+        for name in files:
+            for pattern in patterns:
+                if fnmatch.fnmatch(name, pattern):
+                    yield os.path.join(path, name)
+                    break
+        if single_level:
+            break
+
+def set_up():
+    '''
+    filings_dict is a dictionary. a python dictionary is made up of key/value pairs and is a very powerful way to store data.
+    kp_public_affairs is a key in the filingd_dict dictionary and it's associated value is a list of links.
+    i'll use the .items() method of the dictionary to loop through the key/value pairs to create a directory structure to download the PDF disclosure reports into
+    '''
+    filings_dict = {
+        'kp_public_affairs': [
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1707953&amendid=0',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1678917&amendid=0',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1657226&amendid=0',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1637112&amendid=0',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1620449&amendid=0',
+        ],
+        'nielsen': [
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1708226&amendid=0',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1678882&amendid=1',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1656554&amendid=0',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1636552&amendid=0',
+        ],
+        'lang': [
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1709975&amendid=0',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1679100&amendid=1',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1657179&amendid=1',
+            'http://cal-access.ss.ca.gov/PDFGen/pdfgen.prg?filingid=1633568&amendid=0',
+        ],
+    }
+    
+    main_dir_name = 'lobbying_reports'
+    if os.path.isdir(main_dir_name): # test to see if the folder exists
+        try:
+            cmd = 'rm -rf %s' % main_dir_name # try deleteing the folder the nix way
+            os.system(cmd)
+        except:
+            cmd = 'rmdir /S %s' % main_dir_name # if the above fails try the Windows command line way. If both fail, the script fails
+            os.system(cmd)
+    
+    os.mkdir(main_dir_name) # create or re-create the main directory
+    for lobbyist,link_list in filings_dict.items(): # loop through the filings dict contents, assigning the key to the variable lobbyist and the value to the variable file_list
+        lobbyist_folder_path = os.path.join(main_dir_name, lobbyist) # establish where the lobbying reports for this lobbyist will live
+        os.mkdir(lobbyist_folder_path) # now create that folder
+        i = 1 # get a counter variable ready to roll. i'm just gonna name the report like so: lobbyist_name_1, lobbyist_name_2, etc
+        for link in link_list: # loop through the report links for this lobbyist
+            
+            remote_file_request = requests.post(link) # request the PDF from the server
+            remote_file_content = remote_file_request.content # store the contents of the PDF file
+            
+            local_file_name = '%s_%s.pdf' % (lobbyist, i) # create a local file name to use
+            outfile_path = os.path.join(lobbyist_folder_path, local_file_name) # establish the path to where that file will live
+            outfile = open(outfile_path, 'wb') # open a file handle to write to
+            outfile.write(remote_file_content) # write the contents of the file out to the file
+            outfile.close() # close the file
+            
+            i += 1 # increase the report count by one
+
+def textify():
+    file_list = list(all_files('lobbying_reports', '*.pdf')) # use the all_files function to find all the pdf files and use the python list function to make it an actual list
+    for file in file_list:
+        cmd = 'pdftotext -layout %s' % file
+        os.system(cmd)
+
+def list_files(type):
+    file_list = list(all_files('lobbying_reports', '*.%s' % type))
+    return file_list
+
+def look_for_bills_lobbied():
+    # this is just an example of how you could troll through a directory of files looking for stuff
+    # my 30 second solution to finding the bills could definitley be improved
+    text_files = list_files('txt')
+    for file in text_files:
+        infile = open(file)
+        lobbyist = file.split('/')[1]
+        keep_going = False
+        #for line in infile:
+        for line in lines:
+            if keep_going == False:
+                if re.search(r'Schedule\W+F625', line.strip()):
+                    print '%s\t%s' % (lobbyist, line.strip())
+                    keep_going = True
+            elif line == '\n':
+                keep_going = False
+            else:
+                print '%s\t%s' % (lobbyist, line.strip())
+        infile.close()
